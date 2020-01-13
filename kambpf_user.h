7c38614e29669d5c21576a18c1de0fb5e7579ffb
Dusan Zivanovic <zdule1998@gmail.com>
2020-01-13 01:55:50 +0100

    Include <asm/types.h> for users

--- kambpf_user.h
+++ kambpf_user.h
@@ -1,10 +1,10 @@
 #ifndef KAMBPF_USER_H
 #define KAMBPF_USER_H
 
-#include <stdint.h>
+#include <asm/types.h>
 
-typedef uint32_t __u32;
-typedef uint64_t __u64;
+//typedef uint32_t __u32;
+//typedef uint64_t __u64;
 
 struct _probe_table_empty_entry {};
 
